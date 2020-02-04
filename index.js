085adef021dc7d2ff198454e127a4eade52270e6
robert <robert.langereis@gmail.com>
2020-02-04 11:37:14 +0100

    step two, scrape garfield website

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: false });
 
-const URL = 'http://blog.oscarmorrison.com/nightmarejs-on-heroku-the-ultimate-scraping-setup/';
+const URL = 'https://www.gocomics.com/garfield';
 console.log('Welcome to Nightmare scrape\n==========');
 
 const run = async function() {
-    const webScrape = await nightmare
-    .goto(URL)
-    .wait('.post-title')
-    .evaluate(() => document.querySelector('.post-title').textContent)
-    .end()
-    .then((result) => {
-        console.log(result);
-        console.log('=========\nAll done');
-    })
-    .catch((error) => {
-        console.error('an error has occurred: ' + error);
-    })
-    .then(() => (console.log('process exit'), process.exit()));
+	const webScrape = await nightmare
+		.goto(URL)
+		.wait('.gc-card__image.gc-card__image--overlay')
+		.evaluate(() => document.querySelector('body').innerHTML)
+		// .end()
+		.then(response => console.log(response))
+		// .then(result => {
+		// 	return nightmare
+		// 		.goto(`${result}`)
+		// 		.screenshotSelector({ selector: 'img', path: `./image/${date}.png` })
+		// 		.end(console.log('webscraper done'));
+		// })
+		.catch(error => {
+			console.log(error, 'ERROR');
+		});
+
+
     let finalResult =
     webScrape
 return await finalResult;
