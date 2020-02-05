ea700275147756ddc477af1221f7e9c464cfaf02
robert <robert.langereis@gmail.com>
2020-02-05 17:33:27 +0100

    heroku doesn't work sending the email - even though it works locally. Console.logging the emails and password that heroku gets from it's env variables

--- index.js
+++ index.js
@@ -157,10 +157,10 @@ const run = async function() {
 			console.log('deleted 2nd drawing!')
 		);
 		await fs.unlink(`./image/${date}-3.png`, () =>
-			console.log('deleted 3rd drawing!')
-			console.log('sender_email', sender_email);
-			console.log('email_password', email_password);
-			console.log('receiver_email', receiver_email);
+			console.log('deleted 3rd drawing!'),
+			console.log('sender_email', sender_email),
+			console.log('email_password', email_password),
+			console.log('receiver_email', receiver_email)
 		);
 	};
 
